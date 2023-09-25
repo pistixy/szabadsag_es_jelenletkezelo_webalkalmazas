1a74ff5d1043e1c17c2c17d21d2c784e119a0bf6
pistixy <71815251+pistixy@users.noreply.github.com>
2023-09-25 09:28:28 +0200

    hibak nagyreszt kijavitva

---
+++ logout.php
@@ -0,0 +1,20 @@
+<?php
+session_start(); // Start the session
+
+// Check if the user is logged in
+if (isset($_SESSION['logged'])) {
+    // Unset all session variables
+    $_SESSION = array();
+
+    // Destroy the session
+    session_destroy();
+
+    // Redirect to the homepage or any other suitable page
+    header("Location: index.php"); // You can change "index.php" to the desired page
+    exit;
+} else {
+    // If the user is not logged in, you can handle this case (e.g., redirect to login page)
+    header("Location: includes/loginn.php"); // Redirect to the login page
+    exit;
+}
+?>
--- nav-bar.php
+++ nav-bar.php
@@ -1,37 +1,37 @@
 <?php
+session_start();
 include "includes/connect.php";
 ?>
+
 <div class="navbar">
     <div class="logo">
         <a href="index.php">
-            <img src="includes/logo.png">
+            <img src="includes/logo.png" alt="Logo">
         </a>
     </div>
     <div class="navbar_items">
-        <?php if (isset($_SESSION['email'])) {
-            echo "be vagy jelentkezve";
+        <?php
+        if (isset($_SESSION['logged'])) {
+            echo '<a href="includes/profil.php">Naptáram</a>';
+        } else {
         }
         ?>
     </div>
     <div class="navbar_items">
-    Search users
-    </div>
-
-    <div class="navbar_items">
-        <?php if (isset($_SESSION['logged'])) {
-            echo '<a href="includes/profil.php">' . $email . '</a>';
-        }
-        else{
+        <?php
+        if (isset($_SESSION['logged'])) {
+            echo '<a href="includes/profil.php">' . $_SESSION['email'] . '</a>';
+        } else {
             echo '<a href="includes/loginn.php">Bejelentkezés</a>';
         }
         ?>
     </div>
 
     <div class="navbar_items">
-        <?php if (isset($_SESSION['logged'])) {
+        <?php
+        if (isset($_SESSION['logged'])) {
             echo '<a href="logout.php">Kijelentkezés</a>';
-        }
-        else{
+        } else {
             echo '<a href="includes/registeration.php">Regisztráció</a>';
         }
         ?>
