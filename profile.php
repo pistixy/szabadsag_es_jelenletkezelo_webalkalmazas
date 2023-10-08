3a9af3821598cee60d3045337eaaf3c3cad0017d
pistixy <71815251+pistixy@users.noreply.github.com>
2023-10-08 07:35:42 +0200

    profilkep megjelenitese

--- profile.php
+++ profile.php
@@ -26,19 +26,29 @@ if ($result->num_rows > 0) {
     <body>
     <?php include "nav-bar.php"; ?>
     <div class="profile-container">
-        <form action="upload_profile_picture.php" method="POST" enctype="multipart/form-data">
+        <div class="profile-picture">
+            <?php
+            if (!empty($row['profile_picture'])) {
+                $profilePicturePath = 'profile_pictures/' . $row['profile_picture'];
+                echo '<img src="' . $profilePicturePath . '" alt="Profile Picture">';
+            }
+            ?>
+        </div>
+        <div class="profile-details">
+            <h1 class="profile-heading">Profilod</h1>
+            <p class="profile-data"><strong>Email:</strong> <?php echo $row['email']; ?></p>
+            <p class="profile-data"><strong>Név:</strong> <?php echo $row['name']; ?></p>
+            <p class="profile-data"><strong>Családnév:</strong> <?php echo $row['surname']; ?></p>
+            <p class="profile-data"><strong>Telszám:</strong> <?php echo $row['phone']; ?></p>
+            <p class="profile-data"><strong>Születési dátum:</strong> <?php echo $row['birthdate']; ?></p>
+            <p class="profile-data"><strong>WORKID:</strong> <?php echo $row['WORKID']; ?></p>
+            <a class="edit-profile-link" href="edit_profile.php">Edit Profile</a>
+        </div>
+        <form action="upload_profile_picture.php" method="POST" enctype="multipart/form-data" class="profile-picture-upload-form">
             <label>Profilkép feltöltése:</label>
-            <input type="file" accept="image/gif" accept="image/jpg" accept="image/png" accept="image/jpeg" name="profile_picture" value="profile_picture">
-            <input type="submit" name="profile_picture" value="Feltöltés">
+            <input type="file" accept="image/gif, image/jpg, image/png, image/jpeg" name="profile_picture">
+            <input type="submit" name="upload_profile_picture" value="Feltöltés">
         </form>
-        <h1 class="profile-heading">Profilod</h1>
-        <p class="profile-data"><strong>Email:</strong> <?php echo $row['email']; ?></p>
-        <p class="profile-data"><strong>Név:</strong> <?php echo $row['name']; ?></p>
-        <p class="profile-data"><strong>Családnév:</strong> <?php echo $row['surname']; ?></p>
-        <p class="profile-data"><strong>Telszám:</strong> <?php echo $row['phone']; ?></p>
-        <p class="profile-data"><strong>Születési dátum:</strong> <?php echo $row['birthdate']; ?></p>
-        <p class="profile-data"><strong>WORKID</strong>:</strong> <?php echo $row['WORKID']; ?></p>
-        <a class="edit-profile-link" href="edit_profile.php">Edit Profile</a>
     </div>
     </body>
     </html>
