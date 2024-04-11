bc02a8e0825ca889a48f22b653d805f2e1f3fd89
pistixy <istvan.tako2@gmail.com>
2024-04-11 17:26:06 +0200

    FIX - typos

--- days_remaining.php
+++ days_remaining.php
@@ -1,32 +1,19 @@
 <?php
-// Az adatbázis kapcsolatfájl beillesztése
-include "connect.php";
-
+include "connect.php";// Az adatbázis kapcsolatfájl beillesztése
 // Ellenőrizzük, hogy a munka azonosító (work_id) be van-e állítva a munkamenetben
 if (isset($_SESSION['work_id'])) {
     // A munka azonosító (work_id) lekérése a munkamenetből
     $workId = $_SESSION['work_id'];
-
     try {
-        // Készítsünk egy SQL utasítást a fizetett és korábban fizetett szabadságok lekérdezésére az adatbázisból
+        // Készítsünk egy SQL utasítást a fizetett szabadságok lekérdezésére az adatbázisból
         $sql = "SELECT paid_free FROM users WHERE work_id = :work_id";
         $stmt = $conn->prepare($sql);
-
         // A munka azonosító (work_id) paraméter összekapcsolása
         $stmt->bindParam(':work_id', $workId, PDO::PARAM_INT);
-
-        // A lekérdezés végrehajtása
-        $stmt->execute();
-
-        // Az adatok lekérése
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        // A teljes összeg kiszámítása
-        $total = $row['paid_free'];
-
-        // Az összeg kiíratása
-        echo $total;
-
+        $stmt->execute();// A lekérdezés végrehajtása
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);// Az adatok lekérése
+        $total = $row['paid_free'];// A teljes összeg kiszámítása
+        echo $total;// Az összeg kiíratása
     } catch (PDOException $e) {
         // Kezeljük a hibákat
         echo "Hiba: " . $e->getMessage();
